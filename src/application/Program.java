4c3fe590181ffe6acdddf31dbf67c4e49b93977d
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2021-05-24 20:01:40 -0300

    initial commit

---
+++ src/application/Program.java
@@ -0,0 +1,40 @@
+package application;
+
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program {
+    public static void main(String[] args) {
+
+        //Connection connection = DB.getConnection();
+        //DB.closeConnection();
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try{
+            connection =DB.getConnection();
+
+            statement = connection.createStatement();
+
+            resultSet = statement.executeQuery("select * from department");
+
+            while(resultSet.next()){
+                System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeResulSet(resultSet);
+            DB.closeStatement(statement);
+            DB.closeConnection();
+        }
+    }
+}
